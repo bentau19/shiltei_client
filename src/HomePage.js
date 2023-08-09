9b771f2deaa25dce0a3aecfbb234c1f24bcf319c
bentau19 <bentau19@gmail.com>
2023-08-09 18:01:54 +0300

    coffee example tamplate

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,9 @@ function App() {
   return (
     <div>
       <div class="card">
-        asdf
+      <h1 style={{color: "white", padding:"10px",textDecoration:"underline"}}>coffee</h1>
+      <h2 style={{color:"white"}}>10$</h2>
+      <button class="button-3" >BUY</button>
       </div>
     </div>
   );
