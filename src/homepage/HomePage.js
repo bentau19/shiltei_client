2202a323d6f9594674ef7c6c84bebc3ad1da5f7d
bentau19 <bentau19@gmail.com>
2023-08-10 10:47:56 +0300

    adding creditCard (best on dosnt working yet)

--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -1,11 +1,14 @@
 import './HomePage.css';
+import ItemInfoModal from '../Modal/ItemInfoModal';
+import '../creditCard/creditCard.js'
 function App() {
   return (
     <div>
+ 
       <div class="card">
       <h1 style={{color: "white", padding:"10px",textDecoration:"underline"}}>coffee</h1>
       <h2 style={{color:"white"}}>10$</h2>
-      <button class="button-3" >BUY</button>
+      <ItemInfoModal/>
       </div>
     </div>
   );
