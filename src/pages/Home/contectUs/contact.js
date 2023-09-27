0e91b6b9f45023bc358d23b0535ba9139488e950
bentau19 <bentau19@gmail.com>
2023-09-27 10:55:02 +0300

    texture

--- src/pages/Home/contectUs/contact.js
+++ src/pages/Home/contectUs/contact.js
@@ -2,8 +2,30 @@ import React, { useState } from 'react';
 import './ContactUs.css'; // Import your CSS file for styling
 import Axios from 'axios';
 import { getServerId } from '../../../localStorage';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ContactUs() {
+  const success_notify = (message) =>toast.success(message, {
+    position: "bottom-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    });;
+    const error_notify=(message)=>toast.error(message, {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -22,13 +44,25 @@ function ContactUs() {
   const handleSubmit = (e) => {
     e.preventDefault();
     Axios.post(getServerId() + "/send-feedback", { name:formData.name,email:formData.email
-      ,subject:formData.subject,content:formData.content});
+      ,subject:formData.subject,content:formData.content}).then((res)=>{
+        if (res.status===200){
+          if(res.data=="success"){
+          success_notify("Thank you for contact us!!");
+          }else{
+            error_notify("internet connection error");  
+          }
+        }else{  
+          error_notify("internet connection error");
+        console.log("Try Again Later!")
+      }
+      })
     // You can handle form submission logic here, e.g., sending data to a server
     console.log('Form Data:', formData);
   };
 
   return (
     <div style={{marginBottom:"20px", marginTop:"-20px",width:"60%",float: "left"}} className="contact-us-container">
+      <button onClick={()=>success_notify("Thank you for contact us!!")}>try</button>
       <form onSubmit={handleSubmit}>
         <div className="contact-us-form-group">
          
@@ -83,6 +117,30 @@ function ContactUs() {
         </div>
         <button type="submit" className="contact-us-button">Send</button>
       </form>
+      <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+/>
+  <ToastContainer
+  position="bottom-center"
+  autoClose={5000}
+  hideProgressBar={false}
+  newestOnTop={false}
+  closeOnClick
+  rtl={false}
+  pauseOnFocusLoss
+  draggable
+  pauseOnHover
+  theme="light"
+  />
     </div>
   );
 }
