272f9e4c4713d9186bcc31135554cd9b69ae0aa2
bentau19 <bentau19@gmail.com>
2023-11-05 02:22:07 +0200

    aboutus

--- src/pages/AboutUs/aboutUs.js
+++ src/pages/AboutUs/aboutUs.js
@@ -3,9 +3,9 @@ import leftImg from './aboutUsLeft.png';
 import "../Home/testHome.css"
 export const AboutUs=({windowSize})=>{
     return <div className="rela-block page-section new-section">
-    <div className="rela-block gutter-container">
+    <div className="rela-block ">
       <div className="rela-block section-nav">
-        <h2 id='aboutUs' className="right">עלינו</h2>
+        <h2 id='aboutUs' style={{marginRight:"40px"}} className="right">עלינו</h2>
       </div>
       <img src={leftImg} style={{paddingLeft:"auto"}} alt="shiltei" width={windowSize.innerWidth>850?"50%":"100%"}></img> 
       <img src={rightImg} style={{paddingLeft:"auto"}} alt="shiltei" width={windowSize.innerWidth>850?"50%":"100%"}></img>
