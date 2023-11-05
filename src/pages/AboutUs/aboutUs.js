7099fde83539cb6df8577b56925117c8e8c3b92b
bentau19 <bentau19@gmail.com>
2023-11-05 02:25:58 +0200

    ddd

--- src/pages/AboutUs/aboutUs.js
+++ src/pages/AboutUs/aboutUs.js
@@ -4,7 +4,7 @@ import "../Home/testHome.css"
 export const AboutUs=({windowSize})=>{
     return <div className="rela-block page-section new-section">
     <div className="rela-block ">
-      <div className="rela-block section-nav">
+      <div style={{marginBottom:"0",paddingBottom:"0"}} className="rela-block section-nav">
         <h2 id='aboutUs' style={{marginRight:"40px"}} className="right">עלינו</h2>
       </div>
       <img src={leftImg} style={{paddingLeft:"auto"}} alt="shiltei" width={windowSize.innerWidth>850?"50%":"100%"}></img> 
